e9a8b7ec0c7d698a9ef042684802ad58ce87a75e
Chris Flick <chrisflick@gmail.com>
2019-10-22 14:04:27 -0700

    Now adds employees

--- src/index.js
+++ src/index.js
@@ -143,6 +143,69 @@ function add_role() {
     })
 }
 
+function add_employee() {
+    let employees = [];
+    let roles = [];
+
+    connection.query(`SELECT * FROM role`, function (err, data) {
+        if (err) throw err;
+
+
+        for (let i = 0; i < data.length; i++) {
+            roles.push(data[i].title);
+        }
+
+        connection.query(`SELECT * FROM employee`, function (err, data) {
+            if (err) throw err;
+
+            for (let i = 0; i < data.length; i++) {
+                employees.push(data[i].first_name);
+            }
+
+            inquirer
+                .prompt([
+                    {
+                        name: 'first_name',
+                        message: "what's the employees First Name",
+                        type: 'input'
+                    },
+                    {
+                        name: 'last_name',
+                        message: 'What is their last name?',
+                        type: 'input',
+                    },
+                    {
+                        name: 'role_id',
+                        message: 'What is their role?',
+                        type: 'list',
+                        choices: roles,
+                    }, 
+                    {
+                        name: 'manager_id',
+                        message: "Who is their manager?",
+                        type: 'list',
+                        choices: ['none'].concat(employees)
+                    }
+                ]).then(function({first_name, last_name, role_id, manager_id}) {
+                    let queryText = `INSERT INTO employee (first_name, last_name, role_id`;
+                    if (manager_id != 'none') {
+                        queryText += `, manager_id) VALUES ('${first_name}', '${last_name}', ${roles.indexOf(role_id)}, ${employees.indexOf(manager_id)})`
+                    } else {
+                        queryText += `) VALUES ('${first_name}', '${last_name}', ${roles.indexOf(role_id)})`
+                    }
+                    console.log(queryText)
+
+                    connection.query(queryText, function(err, data) {
+                        if (err) throw err;
+
+                        getJob();
+                    })
+                })
+
+        })
+    })    
+}
+
 function view() {
     inquirer
         .prompt(
